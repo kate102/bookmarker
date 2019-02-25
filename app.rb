debf7c1d95980331dbb47dc5288e6942169d99a0
Kate102 <student@Admins-MacBook-Pro-4.local>
2019-02-25 15:56:21 +0000

    Made the bookmarks print out as a list

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     'Bookmark Manager'
-
   end
 
   get '/bookmarks' do
+    bookmarks = Bookmark.new
+    @x = bookmarks.all
     erb :index
   end
 
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -6,3 +6,10 @@ feature 'Bookmarks home page' do
     expect(page).to have_content('Bookmark Manager')
   end
 end
+
+feature 'List bookmark list' do
+  scenario ' displays bookmark list' do
+    visit '/bookmarks'
+    expect(page).to have_content(['x', 'y', 'z'])
+  end
+end
