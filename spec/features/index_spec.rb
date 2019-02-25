d508f633fe8c7f180c26cf401df7abbaf0224f61
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-25 15:12:44 +0000

    feature test for /bookmarks route

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,8 +1,8 @@
 require './app'
 
-feature 'displays Hello World' do
-  scenario 'displays Hello World under extreme torture' do
-    visit '/'
+feature 'Bookmarks home page' do
+  scenario 'displays Bookmark Manager heading' do
+    visit '/bookmarks'
     expect(page).to have_content('Bookmark Manager')
   end
 end
