cd66b2b8dccac8127369324ed091d082dcb04650
Kate102 <student@Admins-MacBook-Pro-4.local>
2019-02-25 17:21:11 +0000

    refactored

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,30 +2,10 @@ require 'pg'
 
 class Bookmark
 
-  def all
-    begin
-
+  def all 
     con = PG.connect :dbname => 'bookmark_manager', :user => 'student'
-
     rs = con.exec "SELECT * FROM bookmarks"
-
-    urls=[]
-    rs.each do |row|
-      urls.push(row['url'])
-    end
-    return urls
-
-    rescue PG::Error => e
-
-      puts e.message
-
-    ensure
-
-    #  rs.clear if rs
-      con.close if con
-
-    end
-
+    rs.map { |bookmark| bookmark['url'] }
   end
 
 end
